d22bb1ab7480b36d1b3713a47aaee5aac7daaa19
1999Arathy <85410988+1999Arathy@users.noreply.github.com>
2023-07-02 20:50:49 +0530

    Add files via upload

---
+++ question_8.py
@@ -0,0 +1,7 @@
+#Write a Python program to get the 4th element and 4th element from last of a tuple.
+
+
+tup = (4,8,"hai",9,456,25,78,91,100,10,"welcome")
+length = len(tup)
+print("4 th element from last is:",tup[-4]," and 4th element from starting is :",tup[3])
+
